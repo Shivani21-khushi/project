c7a0782bf982cd47309348edc5ab10c9eb9263eb
Shivani21-khushi <117021998+Shivani21-khushi@users.noreply.github.com>
2022-12-10 16:06:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-var loaction = document.querySelector(".location-heading");
-var lat = document.querySelector(".lat-heading");
-var long = document.querySelector(".long-heading");
-var timezone = document.querySelector(".time-heading");
-var windspeed = document.querySelector(".Wind-heading");
-var pressure = document.querySelector(".Pressure-heading");
-var humidity = document.querySelector(".Humidity-heading");
-var windDirection = document.querySelector(".Wind-direction-heading");
-var uv =document.querySelector(".uv-heading");
-var feelslike = document.querySelector(".feels-heading");
-
-function getRequestApi() {
-    const apidata = "https://api.weatherstack.com/current?access_key=8cbec4b7aed04afe583c09d1d3330bf0&query=India"
-    fetch(apidata)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data)=>{
-        var loactionname = data['location']['country'];
-        var Lat = data['location']['lat'];
-var time = data["location"]["timezone_id"];
-var wind = data["current"]["wind_speed"];
-var pressurevalue = data["current"]["pressure"];
-var humidityvalue = data["current"]["humidity"];
-var windDirectionvalue = data["current"]["wind_dir"]
-var uvvalue = data["current"]["uv_index"];
-var feelslikevalue = data["current"]["feelslike"];
-var longvalue = data["location"]["lon"]
-        loaction.innerHTML = `Location: ${loactionname}`
-        lat.innerHTML = `Lat: ${Lat}`
-        timezone.innerHTML = `Time Zone:${time}`
-        windspeed.innerHTML = `Wind Speed: ${wind}`
-        pressure.innerHTML = `Pressure:${pressurevalue}`
-        humidity.innerHTML = `Humidity:${humidityvalue}`
-        windDirection.innerHTML = `Wind Direction:${windDirectionvalue}`
-        uv.innerHTML = `UV Index:${uvvalue}`
-        feelslike.innerHTML = `Feels Like:${feelslikevalue}`
-        long.innerHTML = `Long:${longvalue}`
-    })
+let selectedValue = document.getElementById("list").value;
+function getSelectedValue() {
+  selectedValue = document.getElementById("list").value;
 }
+
+const employeeArr = [
+  { Id: 1, Name : "John", Age : "18", Profession : "Developer" },
+  { Id: 2, Name : "Jack", Age : "20", Profession : "Developer" },
+  { Id: 3, Name : "Karen", Age : "19", Profession : "Admin" },
+];
+
+filterEmployee = () => {
+  var filteredArray = employeeArr.filter(filteredEmployee);
+  var text = "";
+  filteredArray.forEach(myFun);
+
+  function myFun(user) {
+    text +=
+       user.Id +
+      "." + " "+
+      "Name"+":"+user.Name +
+       "  "+ 
+     "Profession"+":"+user.Profession +" "+
+      "Age"+":"+user.Age +
+      "<br>";
+  }
+  document.getElementById("item").innerHTML = text;
+  };
+
+filteredEmployee = (employee) => {
+  employee.Age = Number(employee.Age);
+  if (employee.Name == "John") {
+    employee.Age = 18;
+  }
+  console.log(typeof employee.Age);
+
+  if (selectedValue == "Profession") {
+    return true;
+  }
+  return employee.Profession == selectedValue;
+};
+filterEmployee();
